9d0206e7248e98ce9af0b49376453f14bb564389
andreysakharuk <andreysakharuk@gmail.com>
2018-01-31 22:19:20 +0300

    Merge branch 'master' of https://github.com/andreysakharuk/sti into home_branch
    # Conflicts:
    #	src/test/java/sqvr/FirstTest.java

---
+++ src/test/java/sqvr/ChapterOne.java
@@ -0,0 +1,25 @@
+package sqvr;
+
+public class ChapterOne {
+
+  public static void main (String[] args) {
+      int beerNum = 99;
+      String word = "bottles";
+      while (beerNum >= 1) {
+          if (beerNum == 1) {
+              word = "bottle";
+
+          }
+          System.out.println(beerNum + " " + word + " of the beer on the table. ");
+          System.out.println(beerNum + " " + word + " of the beer on the table. ");
+          System.out.println("Take one. ");
+          System.out.println("Ride round. ");
+          beerNum = beerNum - 1;
+          if (beerNum > 0) {
+              System.out.println(beerNum + " " + word + " of the beer on the table. ");
+          } else {
+              System.out.println("There are no bottles on the table.  ");
+          }
+      }
+  }
+}
